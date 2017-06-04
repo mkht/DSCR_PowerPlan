bba53820cb20924d68c047970af6ed1238ed22c7
mkht <mkht@users.noreply.github.com>
2017-06-04 18:10:01 +0900

    Add "Description" property for cPowerPLan #1 Include bugfix Update readme & sample script

--- Sample/PowerPlan.ps1
+++ Sample/PowerPlan.ps1
@@ -9,8 +9,16 @@ Configuration PowerPlan_Sample
         {
             Ensure = "Present"
             GUID = "381b4222-f694-41f0-9685-ff5bb260df2e"
-            Name = "バランス"
+            Name = "Balanced"
             Active = $true
+        }
+
+        cPowerPlan PowerPlan_Sample2
+        {
+            Ensure = "Present"
+            GUID = "893c34ea-563b-4217-a144-c17f69bed8aa"
+            Name = "Customized Plan"
+            Description = "This is Customized"
         }
 
         cPowerPlanSetting PowerPlanSetting_Sample
